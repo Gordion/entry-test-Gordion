1cc8eece51443b872ef8c58ea9d0cac4040ddc40
Gordion <Darthgordion@gmail.com>
2021-09-09 16:17:34 +0300

    Removed comments

--- entry-test-Gordion-main/06-repeatString/index.js
+++ entry-test-Gordion-main/06-repeatString/index.js
@@ -4,18 +4,14 @@
  * @returns {string} - returns the new string
  */
 export const repeatString = (str = '') => {
-
   const result = [];
   const stringArray = str.split("");
-
   stringArray.forEach((item, index) => {
     const letterBlock = [];
-    console.log('item', item);
       for (var i = 0; i < index+1; i++) {
         letterBlock.push(i === 0 ? item.toUpperCase() : item.toLowerCase());
       }
     result.push(letterBlock.join(''));
-    console.log(result);
   });
   return result.join('-');
 }
